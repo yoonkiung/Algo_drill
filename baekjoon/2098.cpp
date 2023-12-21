01f53ee780ad58fca48cb1101e4d647fd52fcc81
yoonkiung <rldnd4429@gmail.com>
2023-12-21 22:32:02 +0900

    DP 알고리즘 고급

--- baekjoon/2098.cpp
+++ baekjoon/2098.cpp
@@ -2,44 +2,37 @@
 #include <vector>
 #include <algorithm>
 #include <limits.h>
+#include <string.h>
 
 using namespace std;
 
 int n;
 int answer = INT_MAX;
 int board[16][16];
-bool visited[16] = {false,};
+int dp[16][1 << 16];
 
-void    dfs(int pos, int cost, int depth)
+int dfs(int cur, int visited)
 {
-    // cout << pos << " ";
-    if (depth == n - 1)
+    if (visited == (1 << n) - 1)
     {
-        
-        if (board[pos][0] != 0)
-        {
-            answer = min(answer, cost + board[pos][0]);
-            // cout << 0 << " ";
-        }   
-        // cout << "\n";
-        return;
+        if (board[cur][0] == 0)
+            return 1e9;
+        else
+            return board[cur][0];
     }
-    for (int next = 1; next < n; next++)
+    if (dp[cur][visited] != -1)
+        return dp[cur][visited];
+    dp[cur][visited] = 1e9;
+
+    for (int next = 0; next < n; next++)
     {
-        if (visited[next] == true)
+        if (board[cur][next] == 0)
             continue;
-        if (board[pos][next] == 0)
+        if ((visited & (1 << next)) == 1 << next)
             continue;
-        visited[next] = true;
-        dfs(next, cost + board[pos][next], depth + 1);
-        visited[next] = false;
-        // if (!visited[next] && board[pos][next] != 0)
-        // {
-        //     visited[next] = true;
-        //     dfs(next, cost + board[pos][next], depth + 1);
-        //     visited[next] = false;
-        // }
+        dp[cur][visited] = min(dp[cur][visited], board[cur][next] + dfs(next, visited | 1 << next));
     }
+    return dp[cur][visited];
 }
 
 int main()
@@ -51,7 +44,8 @@ int main()
         for (int j = 0; j < n; j++)
             cin >> board[i][j];
     }
-    visited[0] = true;
-    dfs(0, 0, 0);
-    cout << answer;
+    memset(dp, -1, sizeof(dp));
+    
+    cout << dfs(0, 1);
+    // cout << answer;
 }
